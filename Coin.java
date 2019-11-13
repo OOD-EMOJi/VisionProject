7fc489bda891286b262c0719cf22f9ec75bbf2ef
Jan Fic <43736433+janfic@users.noreply.github.com>
2019-11-13 14:13:04 -0500

    Create Coin.java

---
+++ Coin.java
@@ -0,0 +1,36 @@
+public class Coin implements Thing{
+	int x, y, value;
+	public Coin( int x, int y, int value) {
+		this.x = x;
+		this.y = y;
+		this.value = value;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
+	
+	public void draw() {
+		return;
+	}
+	
+	public char draw() {
+		return '$';
+	}
+}
