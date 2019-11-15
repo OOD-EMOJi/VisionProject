9f82d69402ec4e751adffb38ca547868e031e103
Jan Fic <43736433+janfic@users.noreply.github.com>
2019-11-15 17:54:53 -0500

    Some comments on ScoutBehavior

--- LeftWallScout.java
+++ LeftWallScout.java
@@ -1,8 +1,12 @@
+import static LepinskiEngine.DirType.*;
 // import command
 public class LeftWallScout extends ScoutBehavior {
+	
+	//make constructor that requires location
+	
     public Command getCommand() {
         List<DirType> directions = currentLocation.getDirections();
-
+		//We think you need to edit this this does not seem to follow the wall?
         if (directions.contains(West))
             return new CommandMove(this, West);
         else if (directions.contains(South))
