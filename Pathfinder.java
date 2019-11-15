cc8a58ea78f21409c98b2c47b4b11275d33ad9f8
Maria shehata <maria.sheahata17@ncf.edu>
2019-11-15 11:32:29 -0500

    updating Pathfinder() to take a start and end tiles

--- Pathfinder.java
+++ Pathfinder.java
@@ -1,5 +1,5 @@
 import java.util.*;
 
-public interface Pathfinder {
-	public List<Tile> findPath();
+public interface Pathfinder {    
+    public List<Tile> findPath(Tile startTile,Tile endTile);
 }
