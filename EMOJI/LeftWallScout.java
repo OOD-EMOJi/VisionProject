1b0b8773190b29181b3639987f48a123614287d7
Ellie <43616100+elllie@users.noreply.github.com>
2019-11-15 20:42:24 -0500

    fixed wall following logic

--- EMOJI/LeftWallScout.java
+++ EMOJI/LeftWallScout.java
@@ -2,17 +2,16 @@ import LepinskiEngine.*;
 import static LepinskiEngine.DirType.*;
 import java.util.*;
 
-public class LeftWallScout extends ScoutBehavior {
-	
+public class LeftWallScout implements RobotBehavior {
+
 	//make constructor that requires location
-	
-    public Command getCommand() {
-        List<DirType> directions = currentLocation.getDirections();
-		//We think you need to edit this this does not seem to follow the wall?
-        if (directions.contains(West))
-            return new CommandMove(null, West);
-        else if (directions.contains(South))
+
+    public Command getCommand(Location location) {
+        List<DirType> directions = location.getDirections();
+        if (directions.contains(South))
             return new CommandMove(null, South);
+        else if (directions.contains(West))
+            return new CommandMove(null, West);
         else if (directions.contains(North))
             return new CommandMove(null, North);
         else
