b0745ac9d452b15d98c1c5b868dc729923896dcf
Jan Fic <43736433+janfic@users.noreply.github.com>
2019-11-20 13:40:03 -0500

    Update CoinBotPathOptionGenerator.java

--- EMOJI/CoinBotPathOptionGenerator.java
+++ EMOJI/CoinBotPathOptionGenerator.java
@@ -9,7 +9,6 @@ public class CoinBotPathOptionGenerator implements PathOptionGenerator {
     private Pathfinder pathfinder;
     private int height;
     private int width;
-	private int firstTimeTurns;
 
     public CoinBotPathOptionGenerator(Maze maze) {
         this.maze = maze;
@@ -17,28 +16,17 @@ public class CoinBotPathOptionGenerator implements PathOptionGenerator {
         this.width = maze.tiles.length;
         this.pathfinder = new BreadthFirstSearchPathFinder(maze);
         pathList = new ArrayList<PathOption>();
-        this.firstTimeTurns=0;
     }
 
     // generatePathOptions takes the robot location and the remaining num of turns
     public List<PathOption> generatePathOptions(int x, int y, int turns) {
-        if (firstTimeTurns==0)firstTimeTurns= turns;
         pathList = new ArrayList<PathOption>();
-        List<Tile> tiles;
-		Comparator<PathOption> comparator;
-        if(turns < (firstTimeTurns/2)){
-            tiles = getCoins();
-			comparator = new PathOption.GetCoinPathOptionComparator();
-        }else{ 
-			tiles = getDeadEndsTiles();
-			comparator = new PathOption.DeadEndPathOptionComparator();
-		}
-        
+        List<Tile> tiles = getDeadEndsTiles();
         for (Tile tile : tiles) {
             PathOption option = new PathOption(pathfinder.findPath(maze.tiles[x][y], tile), turns);
             option.countPoints();
 			
-            if (option.path.size() > 0 && option.earlierFactor>0) {
+            if (option.path.size() > 0) {
                 pathList.add(option);
             }
             //System.out.println(tile.getX() + " " + tile.getY() + " " + option);
@@ -50,31 +38,6 @@ public class CoinBotPathOptionGenerator implements PathOptionGenerator {
             System.out.println(pathOption);
         }
         return pathList;
-    }
-
-    private List<Tile> getCoins() {
-
-        List<Tile> tileList = new ArrayList<Tile>();
-        int[][] SHIFT = {
-            {0, 1}, // going right
-            {1, 0}, // going down
-            {0, -1}, // going left
-            {-1, 0} // going up`
-        };
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                        List<Thing> contents = maze.tiles[x][y].getContents();
-                        for (Thing t : contents) {
-                            if (t instanceof Coin) {
-                                tileList.add(maze.tiles[x][y]);
-                            }
-                        }      
-                                            
-                    
-                
-            }
-        }
-        return tileList;
     }
 
     private List<Tile> getDeadEndsTiles() {
@@ -110,6 +73,4 @@ public class CoinBotPathOptionGenerator implements PathOptionGenerator {
         return tileList;
 
     }
-
-    
 }
