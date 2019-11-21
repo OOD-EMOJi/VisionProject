259cf768abe24935d3b33bc0352a05ff683276b2
Maria shehata <maria.sheahata17@ncf.edu>
2019-11-21 00:09:43 -0500

    Documentation for CoinBotPathOptionGenerator.java

--- EMOJI/CoinBotPathOptionGenerator.java
+++ EMOJI/CoinBotPathOptionGenerator.java
@@ -1,3 +1,26 @@
+/*
+
+This class generates the possible pathOption list and then sort them, so the coin bot decide which 
+one to choose.
+The getCoins() method used to return a list of the all the tiles in the maze that has coins in it.
+The generatePathOptions() method take the x and y positions of the coin bot and the number of turns. 
+It uses the current location of the current bot as the starting tile for each pathOption. 
+
+The main function of generatePathOptions() is to generate the shortest path from the coin bot location
+to each tile that has a coin, using the getCoins() to get the ending tiles. It use Breadth first search to generate the path.
+
+While it generates each path, it calculates the points in the path based on how many coins are there based on the remaining number of turn, 
+in other words how many coins can the bot collect from this path in the amount of remaining turns 
+It also calculates the earlier factor for each path to bused for the sorting comparison if the number of points is the same for two pathOptions
+
+generatePathOptions
+In generatePathOptions():
+1] for each tile that has a coin
+2] generate a path from the bot's tile to the tile that has the coin
+3] add the pathOption to the pathOption list
+4] sort the list and return it
+
+*/
 package EMOJI;
 
 import java.util.*;
