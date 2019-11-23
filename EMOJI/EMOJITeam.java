df1cd6759f6846cd43b9f6c379cb1e7b194928ab
Ellie Young <eleanor.young18@ncf.edu>
2019-11-22 20:26:49 -0500

    changed scouts to W Clockwise and Random

--- EMOJI/EMOJITeam.java
+++ EMOJI/EMOJITeam.java
@@ -50,7 +50,7 @@ public class EMOJITeam implements PlayerTeam {
             if (type == ModelType.CoinBot) {
                 behavior = new CoinBotBehavior(state.turns_remaining, bot, new CoinBotPathOptionGenerator(mazeA));
             } else {
-                behavior = hasLeft ? new LeftWallScout() : new RightWallScout();
+                behavior = hasLeft ? new ClockwiseTwirler(DirType.West) : new RandomScout();
                 hasLeft = true;
             }
             behaviors.put(id, behavior);
